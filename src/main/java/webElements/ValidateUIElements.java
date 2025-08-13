8e326c779d88066439a5926293239aea6bce05cd
Malini2905 <malinir@ugf360.com>
2025-08-13 17:56:30 +0530

    New codes

--- src/main/java/webElements/ValidateUIElements.java
+++ src/main/java/webElements/ValidateUIElements.java
@@ -6,11 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
 public class ValidateUIElements {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args)  {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com//dropdownsPractise//");
 		driver.manage().window().maximize();
-		Thread.sleep(1000);
 		// System.out.println(driver.findElement(By.name("ctl00$mainContent$view_date2")).isEnabled());
 		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
 		driver.findElement(By.name("ctl00$mainContent$view_date2")).click();
